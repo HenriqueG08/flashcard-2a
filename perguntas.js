ce5106d47389a684cf3017dbe99dfeff79320d0a
HenriqueG08 <henrique.guarneri@escola.pr.gov.br>
2025-07-07 11:36:41 -0300

    salvar

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao (
+    Geografia,
+    Qual a cidade mais populosa dos EUA?,
+    Nova York
+)
+
+criaCartao (
+    História,
+    Em que século começou a Revolução Industrial?,
+    Século XVIII
+)
+
+criaCartao (
+    Matemática,
+    Calcule 12 x 15.,
+    180
+)
+
+criaCartao (
+    Geometria,
+    Quantos lados possui um hexágono?,
+    Seis lados
+)
+
+criaCartao (
+    Futebol,
+    Quem ganhou a última Copa do Mundo?,
+    Argentina
+)
+
+criaCartao (
+    Química,
+    Determine o elemento químico representado pelo símbolo C.,
+    Carbono
+)
