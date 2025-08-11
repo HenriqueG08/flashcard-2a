095f8459994fdeea7fb9e6b7da074cb32535dd65
HenriqueG08 <henrique.guarneri@escola.pr.gov.br>
2025-08-11 08:51:38 -0300

    salvar

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao (
-    Geografia,
-    Qual a cidade mais populosa dos EUA?,
-    Nova York
+    'Geografia',
+    'Qual a cidade mais populosa dos EUA?',
+    'Nova York'
 )
 
 criaCartao (
-    História,
-    Em que século começou a Revolução Industrial?,
-    Século XVIII
+    'História',
+    'Em que século começou a Primeira Revolução Industrial?',
+    'Século XVIII'
 )
 
 criaCartao (
-    Matemática,
-    Calcule 12 x 15.,
-    180
+    'Matemática',
+    'Calcule 12 x 15',
+    '180'
 )
 
 criaCartao (
-    Geometria,
-    Quantos lados possui um hexágono?,
-    Seis lados
+    'Geometria',
+    'Quantos lados possui um hexágono?',
+    'Seis lados'
 )
 
 criaCartao (
-    Futebol,
-    Quem ganhou a última Copa do Mundo?,
-    Argentina
+    'Futebol',
+    'Quem ganhou a última Copa do Mundo?',
+    'Argentina'
 )
 
 criaCartao (
-    Química,
-    Determine o elemento químico representado pelo símbolo C.,
-    Carbono
+    'Química',
+    'Determine o elemento químico representado pelo símbolo C.',
+    'Carbono'
 )
